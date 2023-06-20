883d9b76ea328bb7075bc07841743c6772f4fcbb
Your Name <you@example.com>
2023-06-20 03:11:29 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char word[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
