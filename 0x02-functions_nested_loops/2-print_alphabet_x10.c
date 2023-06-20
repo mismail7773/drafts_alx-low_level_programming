e12bd0ce131687525df2d89023ea3d73941f09fe
Your Name <you@example.com>
2023-06-20 05:38:53 -0700

    task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,5 +2,22 @@
 
 /**
  *  print_alphabet_x10 - draft
- *
- *  
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		char j = 'a';
+		while (j < 'z')
+		{
+			_putchar(j);
+			j ++;
+		}
+
+		_putchar('\n');
+		
+		i ++;
+	}
+}
