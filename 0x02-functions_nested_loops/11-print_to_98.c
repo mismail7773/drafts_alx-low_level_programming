004dc00f5da41ae6a5681afa234fcbb7f75f927c
Your Name <you@example.com>
2023-06-20 19:44:25 -0700

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - draft
+ * @n: draft
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		if (n < 98)
+		{
+			for (i = n; i < 98; i++)
+			{
+				printf("%d, ", i);
+			}
+		}
+		else
+		{
+			;
+		}
+	}
+	printf("98\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,5 +18,6 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 int add(int m, int n);
+void print_to_98(int n);
 
 #endif
