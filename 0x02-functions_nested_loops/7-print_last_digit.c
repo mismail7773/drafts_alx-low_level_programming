8fe04c5e79550d341bf55939efef6fd1172cd3c3
Your Name <you@example.com>
2023-06-20 17:14:13 -0700

    task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * print_last_digit - draft
+ * @n: draft
+ *
+ * Return: draft
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	
+	if (m < 0)
+		m = - m;
+
+	_putchar(m + '0');
+
+	return (m);
+}
