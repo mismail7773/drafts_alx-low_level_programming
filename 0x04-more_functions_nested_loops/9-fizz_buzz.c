d8bd0f32ba688b6c4c6aa62580c8286f1ded1cab
Your Name <you@example.com>
2023-06-22 09:13:23 -0700

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,30 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			if (i % 5 == 0)
 			{
-				printf("Buzz ");
+				printf("Buzz");
 			}
 			else
 			{
 				if (i % 3 == 0)
 				{
-					printf("Fizz ");
+					printf("Fizz");
 				}
 				else
 				{
-					printf("%d ", i);
+					printf("%d", i);
 				}
 			}
+		}
+
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 
