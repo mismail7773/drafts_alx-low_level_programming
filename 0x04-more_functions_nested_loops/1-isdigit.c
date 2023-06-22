ba23ff0ad99b945c41311754326cc426fdf8634a
Your Name <you@example.com>
2023-06-22 06:09:42 -0700

    task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - draft
+ *
+ * @c: draft
+ *
+ * Return: draft
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	if (c >= 48 && c <= 57)
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+
+	return (i);
+}
