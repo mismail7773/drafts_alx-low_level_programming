fe101019bc71600d91eea9a6b6dd86e9550d23a5
Your Name <you@example.com>
2023-06-22 09:02:02 -0700

    task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - draft
+ *
+ * Return: draft
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				if (i % 3 == 0)
+				{
+					printf("Fizz");
+				}
+				else
+				{
+					printf("%d", i);
+				}
+			}
+		}
+
+		printf(" ");
+	}
+	
+	printf("\n");
+
+	return (0);
+}
