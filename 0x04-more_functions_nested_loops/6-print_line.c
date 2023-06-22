43590bb34606d770261718238eb8a18db08fee2e
Your Name <you@example.com>
2023-06-22 07:34:48 -0700

    task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draft
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i<= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}
