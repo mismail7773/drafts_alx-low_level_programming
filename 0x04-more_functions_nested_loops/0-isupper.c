065830a3233e3fe873f1d3dd82ed5053dd740b13
Your Name <you@example.com>
2023-06-22 06:00:55 -0700

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - draft
+ *
+ * @c: draft
+ *
+ * Return: draft
+ */
+int _isupper(int c)
+{
+	int i;	
+
+	if (c >= 65 && c <= 90)
+	{
+		i = 1;
+	}
+	else
+	{
+		i = 0;
+	}
+
+	return (i);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef draft
+#define draft
+
+/**
+ * ISD - draft
+ * 
+ * @c: draft
+ *
+ * Return: draft
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
