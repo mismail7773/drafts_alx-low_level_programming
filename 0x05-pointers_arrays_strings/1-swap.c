33e66e1bd053c834b6caeb145e9804f5c743861d
Your Name <you@example.com>
2023-07-01 15:15:13 -0700

    task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - draft
+ * @a: draft
+ * @b: draft
+ */
+void swap_int(int *a, int *b)
+{
+	int *n;
+
+	n = a;
+	a = b;
+	b = n;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strlen - draft
+ * @s: draft
+ */
+int _strlen(char *s)
+{
+
