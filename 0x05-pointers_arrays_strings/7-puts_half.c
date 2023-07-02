0713f8a13ef46e3040dc1d1fbb70bedb8e5feec1
Your Name <you@example.com>
2023-07-02 01:38:28 -0700

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - draft
+ * @str: draft
+ */
+void puts_half(char *str)
+{
+	int i; j;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = (i - 1); j >= (i / 2); j--)
+		       _putchar(str[j]);	
+	}
+	else
+	{
+		for (j = (i - 1); j >= ((i + 1) / 2); j--)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
