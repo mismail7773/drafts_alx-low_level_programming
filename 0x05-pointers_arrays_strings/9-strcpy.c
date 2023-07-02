274ba2fbea2a7e377f3beb532c781b70132839b3
Your Name <you@example.com>
2023-07-02 02:16:55 -0700

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * _strcpy - draft
+ * @dest: drraft
+ * @src: draft
+ *
+ * Return: draft
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (src[i != '\0')
+		i++;
+	for (j = 0; j < i - 1; j++)
+	       dest[j] = src[j];
+	dest[j] = src[j]
+}	
