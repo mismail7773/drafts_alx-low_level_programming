0ac18a85e32a6856b49251f9fef68edd1a0e34a2
Your Name <you@example.com>
2023-07-02 01:44:00 -0700

    task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 0)
 	{
-		for (j = (i - 1); j >= (i / 2); j--)
+		for (j = (i / 2); j <= (i - 1 ); j++)
 		       _putchar(str[j]);	
 	}
 	else
 	{
-		for (j = (i - 1); j >= ((i + 1) / 2); j--)
+		for (j = ((i + 1) / 2); j <= (i - 1); j++)
 			_putchar(str[j]);
 	}
 	_putchar('\n');
