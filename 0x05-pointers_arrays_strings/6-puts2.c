65d565305338edb47eb9e68889ae22a0af5eaa01
Your Name <you@example.com>
2023-07-01 18:18:39 -0700

    task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - draft
+ * @str: draft
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= (i - 1); j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
+
