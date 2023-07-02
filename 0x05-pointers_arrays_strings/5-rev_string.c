445a88597af55b83cb27e087ae935ab84b93b09a
Your Name <you@example.com>
2023-07-01 17:54:52 -0700

    task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * rev_string - draft
+ * print_rev - draft
  * @s: draft
  */
 void rev_string(char *s)
 {
-	int i, j;
+        int i, j, k;
+	char a, b;
 
-	i = 0;
-	while (*(s + i) != '\0')
+        i = 0;
+        while (s[i] != '\0')
+        {
+                i++;
+        }
+
+	j = i - 1;
+	k = 0;
+	for (;j > k; j--)
 	{
-		i++;
+		a = s[k];
+		b = s[j];
+		s[k] = b;
+		s[j] = a;
+		k++;
 	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(*(s + j));
-	}
-	_putchar('\n');
 }
