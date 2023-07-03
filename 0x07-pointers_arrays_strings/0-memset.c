ee765ee84eb9a2d800d1f0e017bf77775b929cc6
Your Name <you@example.com>
2023-07-03 02:49:20 -0700

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - draft
+ * @s: draft
+ * @b: draft
+ * @n: draft
+ *
+ * Return: draft
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		s[i]= b;
+	}
+	return (s);
+}
