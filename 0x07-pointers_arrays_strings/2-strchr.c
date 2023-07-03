e6176e9b8d5de46f537e9b5df627866d449ae866
Your Name <you@example.com>
2023-07-03 10:08:52 -0700

    task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - draft
+ * @s: draft
+ * @c: draft
+ *
+ * Return: draft
+ */
+char *_strchr(char *s, char c)
+{
+	
+	int i, j;
+	
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] == c)
+			break;
+	}
+	return (&s[j]);
+}
