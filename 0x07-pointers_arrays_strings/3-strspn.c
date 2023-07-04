82562782dbf89dc1253a5b1215dd151090910a61
Your Name <you@example.com>
2023-07-04 11:30:29 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, k, l, m, n, o, p;
+	int i, j, k, m, n, o, p;
 
 	i = 0;
 	while (s[i] != '\0')
@@ -17,42 +17,36 @@ unsigned int _strspn(char *s, char *accept)
 	j = 0;
 	while (accept[j] != '\0')
 		j++;
-	l = 0;
 	for (k = 0; k < j; k++)
 	{
 		if (s[0] == accept[k])
 		{
-			l++;
-			break;
-		}
-	}
-	if (l == 1)
-	{
-		n = 0;
-		o = 0;
-		for (m = 0; m < i; m++)
-		{
-			o++;
-			if ((o - n) == 1)
+			n = 0;
+			o = 0;
+			for (m = 0; m < i; m++)
 			{
-				for (p = 0; p < j; p++)
+				o++;
+				if ((o - n) == 1)
 				{
-					if (s[m] == accept[p])
+					for (p = 0; p < j; p++)
 					{
-				       		n++;
-						break;
+						if (s[m] == accept[p])
+						{
+							n++;
+							break;
+						}
 					}
 				}
-			}
-			else
-			{
-				break;
+				else
+				{
+					break;
+				}
 			}
 		}
-	}
-	else
-	{
-		n = 0;
+		else
+		{
+			n = 0;
+		}
 	}
 	return (n);
 }
