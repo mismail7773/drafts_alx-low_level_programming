4bcdd7e0e0c9ef47539fb5f299c71582dc9b8603
Your Name <you@example.com>
2023-07-06 00:02:59 -0700

    task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion0 - draft
+ * @n: draft
+ * @c: draft
+ * Return: draft
+ */
+int _sqrt_recursion0(int n, int c)
+{
+	if (c * c > n)
+		return (-1);
+	if (n == c * c)
+	{
+		return (c);
+	}
+	else
+	{
+		c++;
+		return (_sqrt_recursion0(n, c));
+	}
+}
+
+/**
+ * _sqrt_recursion - draft
+ * @n: draft
+ * Return: draft
+ */
+int _sqrt_recursion(int n)
+{
+	int c;
+
+	c = 2;
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (_sqrt_recursion0(n, c));
+}
