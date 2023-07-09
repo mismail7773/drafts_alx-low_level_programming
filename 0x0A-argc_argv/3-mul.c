06286f36dca0f279eeefaa49f1f702e0b60a0771
Your Name <you@example.com>
2023-07-09 08:33:49 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - draft
+ * @argc: draft
+ * @argv: draft
+ * Return: draft
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	if (atoi(*(argv + 2)) == '\0' || atoi(*(argv + 1)) == '\0')
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
