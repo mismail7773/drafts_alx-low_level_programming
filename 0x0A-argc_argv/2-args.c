10f2db7d13d912f6220fc4b48d79ca7c1c17383b
Your Name <you@example.com>
2023-07-09 06:19:33 -0700

    task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - draft
+ * @argc: draft
+ * @argv: draft
+ * Return: draft
+ */
+int main(int argc, char ** argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
