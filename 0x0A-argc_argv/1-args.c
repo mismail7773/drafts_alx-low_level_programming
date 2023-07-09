09e71d8ed52985f4cd157f1f577f4fcfceff9a05
Your Name <you@example.com>
2023-07-09 06:04:43 -0700

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - draft
+ * @argc: draft
+ * @argv: draft
+ * Return: draft
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
