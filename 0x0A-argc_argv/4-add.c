dda62934c912eb7a84c13a4eba020406cd2a1c2f
Your Name <you@example.com>
2023-07-09 09:30:59 -0700

    task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - draft
+ * @argc: draft
+ * @argv: draft
+ * Return: draft
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}		
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
