161e624b73998115333adbdb28efd168fa824462
Your Name <you@example.com>
2023-07-11 14:04:37 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 	a = malloc(size);
+	if (a == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
-	}
-	if (a == NULL)
-		return (NULL);
+	}	
 	return (a);
 }
