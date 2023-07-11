eb7627eca7ca3dec8602bcb55b9c6b451ad23656
Your Name <you@example.com>
2023-07-11 16:18:46 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,16 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int i, j, k, l;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
 	j = 0;
 	while (s2[j] != '\0')
 		j++;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	s3 = malloc(sizeof(char) * ((i) + (1 + j)));
 	if (s3 == NULL)
 		return (NULL);
@@ -29,10 +29,7 @@ char *str_concat(char *s1, char *s2)
 	k = l + j;
 	for (; i >= 0; --i)
 		s3[i] = s1[i];
-	for (; k >= l; k--)
-	{
+	for (; k >= l; k--, j--)
 		s3[k] = s2[j];
-		j--;
-	}
 	return (s3);
 }
