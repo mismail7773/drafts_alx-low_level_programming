46c38d0ee36d618b01088b575c686675aa81d962
Your Name <you@example.com>
2023-07-11 16:02:01 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	while (s2[j] != '\0')
 		j++;
-	if (i == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (j == 0)
+	if (s2 == NULL)
 		s2 = "";
 	s3 = malloc(sizeof(char) * ((i) + (1 + j)));
 	if (s3 == NULL)
