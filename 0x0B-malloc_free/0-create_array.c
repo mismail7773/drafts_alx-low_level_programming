7653d0b79a75a33ac5d82aadc0d6c9337703892e
Your Name <you@example.com>
2023-07-11 13:59:36 -0700

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - draft
+ * @size: draft
+ * @c: draft
+ * Return: draft
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return ('\0');
+	a = malloc(size);
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
