f65ba12cc2a7f73c275fb38bfd5d6daefe6d6f99
Your Name <you@example.com>
2023-07-11 18:04:19 -0700

    task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - draft
+ * @width: draft
+ * @height: draft
+ * Return: draft
+ */
+int **alloc_grid(int width, int height)
+{
+	int **n, r, c;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	n = malloc(sizeof(int *) * height);
+	if (n == NULL)
+		return (NULL);
+	r = 0;
+	c = 0;
+	for (r = 0; r < height; r++)
+	{
+		n[r] = malloc(sizeof(int) * width);
+		if (n[r] == NULL)
+			return (NULL);
+	}
+	for (r = 0; r < height; r++)
+	{
+		for (c = 0; c < width; c++)
+			n[r][c] = 0;
+	}
+	return (n);
+}
+
