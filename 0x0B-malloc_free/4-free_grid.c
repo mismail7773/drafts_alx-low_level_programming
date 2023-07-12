29104c50148dd211f9dbaed5ca51960df34bdc5b
Your Name <you@example.com>
2023-07-11 19:10:43 -0700

    task

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - draft
+ * @grid: draft
+ * @height: draft
+ * Return: draft
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
