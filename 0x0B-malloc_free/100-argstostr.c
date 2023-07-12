f37bdbedc246c2bd19104d7dbe43440d156b049f
Your Name <you@example.com>
2023-07-12 02:01:45 -0700

    task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - draft
+ * @ac: draft
+ * @av: draft
+ * Return: draft
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i, j, c, k;
+
+	if (ac == 0)
+		return (NULL);
+	i = 0;
+	c = 0;
+	for (; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
+	s = malloc((c + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 1; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+			s[k] = av[i][j];
+		s[k] = '\n';
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
+}
