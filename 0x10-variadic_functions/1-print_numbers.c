a09d7285393130e6364d2d1fc6bf4eb8925d50f5
Your Name <you@example.com>
2023-07-21 06:37:04 -0700

    task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - draft
+ * @separator: draft
+ * @n: draft
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list p;
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(p, int));
+		if (separator && i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(p);
+
+}
