d77318055ea7b11eed599115e8df9191b34f62bb
Your Name <you@example.com>
2023-07-21 06:22:07 -0700

    task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,11 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_list p;
 	unsigned int i, sum;
 
-	if(n == 0)
+	if (n == 0)
 		return (0);
 	va_start(p, n);
 	sum = 0;
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(p, int);
 	va_end(p);
 	return (sum);
